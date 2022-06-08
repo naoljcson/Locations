b0cc2b3cf6e42d16464e3b4d6ff0f10399612b43
Naol Hussien <naol.h@suntecholding.com>
2022-06-08 08:57:35 +0300

    Initial Commit

---
+++ Location/LocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationApp.swift
+//  Location
+//
+//  Created by Naol Hassen on 08/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct LocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
